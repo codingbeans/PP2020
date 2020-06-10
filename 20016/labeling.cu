2ed59023731f7346c9e1cbca8bbcb0b2e4203ee9
codingbeans <asd768999@gmail.com>
2020-06-11 02:07:15 +0800

    Try 20016

---
+++ 20016/labeling.cu
@@ -0,0 +1,44 @@
+#include "labeling.h"
+#include <thrust/tabulate.h>
+#include <thrust/scan.h>
+#include <thrust/functional.h>
+#include <thrust/execution_policy.h>
+
+// Negate struct defined in thrust as an example
+// template<typename T>
+// struct negate
+//  {
+//    typedef T argument_type;
+ 
+//    typedef T result_type;
+ 
+//    __thrust_exec_check_disable__
+//    __host__ __device__ T operator()(const T &x) const {return -x;}
+//  }; // end negate
+ 
+template<typename T>
+struct str2tag {
+    typedef T argument_type;
+    typedef T result_type;
+    const char* cuStr;
+    tagging(const char* cuStr): cuStr(cuStr) {}
+   __thrust_exec_check_disable__
+   __host__ __device__ T operator()(const T &index) const { return (cuStr[index] != ' ') ? -1 : index;}
+};
+
+ 
+template<typename T>
+struct tag2ans {
+    typedef T argument_type;
+    typedef T result_type;
+    const int32_t* cuPos;
+    tagging(const char* cuPos): cuPos(cuPos) {}
+   __thrust_exec_check_disable__
+   __host__ __device__ T operator()(const T &index) const { return index - cuPos[index];}
+};
+
+void labeling(const char *cuStr, int *cuPos, int strLen) {
+	thrust::tabulate(thrust::device, cuPos, cuPos+strLen, str2tag<int32_t>(cuStr));
+	thrust::inclusive_scan(thrust::device, cuPos, cuPos+strLen, cuPos, thrust::maximum<int>());
+	thrust::tabulate(thrust::device, cuPos, cuPos+strLen, tag2ans<int32_t>(cuPos));
+}
