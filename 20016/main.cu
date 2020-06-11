9d40f1777faa4f77f7238b84ea395c7ec684ca74
codingbeans <asd768999@gmail.com>
2020-06-11 17:40:08 +0800

    Add device check

--- 20016/main.cu
+++ 20016/main.cu
@@ -11,6 +11,12 @@ namespace {
 }
  
 int main() {
+    int deviceCount = 0;
+    cudaGetDeviceCount(&deviceCount);
+    if (deviceCount <= 1) {
+        printf("error: invalid device chosen\n");
+        return 0;
+    }
     cudaSetDevice(1);
     static const int MAXN = 16777216<<1;
     static const int MAXTOKEN = 500;
