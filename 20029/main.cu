7b78b00ece74a650c24a0a3bbafea60b1bb8602b
codingbeans <asd768999@gmail.com>
2020-06-13 20:48:25 +0800

    Try

--- 20029/main.cu
+++ 20029/main.cu
@@ -4,30 +4,38 @@
 #define UINT uint32_t
 #define MAXN 1024
 #define MAXCASE 512
-#define MAXGPU 1
 
 #define DEBUG
 
-// __global__
+__global__
 void multiply(int N, UINT A[][MAXN], UINT B[][MAXN], UINT C[][MAXN]) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+    int row = blockIdx.y*blockDim.y + threadIdx.y;
+    int col = blockIdx.x*blockDim.x + threadIdx.x;
+    // for (int i = 0; i < N; i++) {
+    //     for (int j = 0; j < N; j++) {
+    if (row < N && col < N) {
             UINT sum = 0;    // overflow, let it go.
             for (int k = 0; k < N; k++)
-                sum += A[i][k] * B[k][j];
-            C[i][j] = sum;
-        }
+                sum += A[row][k] * B[k][col];
+            C[row][col] = sum;
     }
+    //     }
+    // }
 }
 
-// __global__
+__global__
 void add(int N, UINT A[][MAXN], UINT B[][MAXN], UINT C[][MAXN]) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++)
-            C[i][j] = A[i][j] + B[i][j];
+    int row = blockIdx.y*blockDim.y + threadIdx.y;
+    int col = blockIdx.x*blockDim.x + threadIdx.x;
+    // for (int i = 0; i < N; i++) {
+    //     for (int j = 0; j < N; j++)
+    if (row < N && col < N) {
+            C[row][col] = A[row][col] + B[row][col];
     }
+    // }
 }
 
+__global__
 void rand_gen(UINT c, int N, UINT A[][MAXN]) {
     UINT x = 2, n = N*N;
     for (int i = 0; i < N; i++) {
@@ -38,44 +46,68 @@ void rand_gen(UINT c, int N, UINT A[][MAXN]) {
     }
 }
 
-UINT signature(int N, UINT A[][MAXN]) {
+__global__
+UINT signature(int N, UINT A[][MAXN], UINT* ans) {
     UINT h = 0;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++)
             h = (h + A[i][j]) * 2654435761LU;
     }
+    *ans = h;
     return h;
 }
 
-UINT D_ANS[MAXCASE][2];
+__device__ UINT D_ANS[MAXCASE][2];
+UINT ANS[MAXCASE][2];
 
 void solve(int tc, int N, UINT seedA, UINT seedB) {
-    static UINT IN[2][MAXN][MAXN], TMP[6][MAXN][MAXN];
-    #pragma omp parallel
-    {
-        rand_gen(seedA, N, IN[0]);
-        rand_gen(seedB, N, IN[1]);
-    }
-    #pragma omp parallel
-    {
-        // AB
-        multiply(N, IN[0], IN[1], TMP[0]);
-        // BA
-        multiply(N, IN[1], IN[0], TMP[1]);
-        // AB+BA
-        add(N, TMP[0], TMP[1], TMP[2]);
-        
-        // ABA
-        multiply(N, TMP[0], IN[0], TMP[3]);
-        // BAB
-        multiply(N, TMP[1], IN[1], TMP[4]);
-        // ABA+BAB
-        add(N, TMP[3], TMP[4], TMP[5]);
-    }
+    // UINT IN[2][MAXN][MAXN], TMP[6][MAXN][MAXN];
+    // UINT *IN, *TMP;
+    UINT *D_IN, *D_TMP;
+    // IN = (UINT*)malloc(2*MAXN*MAXN*sizeof(UINT));
+    // TMP = (UINT*)malloc(6*MAXN*MAXN*sizeof(UINT));
 
-    D_ANS[tc][0] = signature(N, TMP[2]);
-    D_ANS[tc][1] = signature(N, TMP[5]);
+    cudaMalloc(&D_IN, 2*N*N*sizeof(UINT));
+    cudaMalloc(&D_TMP, 6*N*N*sizeof(UINT));
+
+    // #pragma omp parallel
+    // {
+        // rand_gen(seedA, N, IN[0]);
+        // rand_gen(seedB, N, IN[1]);
+        rand_gen<<<1, 1>>>(seedA, N, D_IN[0]);
+        rand_gen<<<1, 1>>>(seedB, N, D_IN[1]);
+    // }
+
+
+    // AB
+    // multiply(N, IN[0], IN[1], TMP[0]);
+    multiply<<<N, N>>>(N, D_IN[0], D_IN[1], D_TMP[0]);
+    // BA
+    // multiply(N, IN[1], IN[0], TMP[1]);
+    multiply<<<N, N>>>(N, D_IN[1], D_IN[0], D_TMP[1]);
+    // AB+BA
+    // add(N, TMP[0], TMP[1], TMP[2]);
+    add<<<N, N>>>(N, D_TMP[0], D_TMP[1], D_TMP[2]);
+    
+    // ABA
+    // multiply(N, TMP[0], IN[0], TMP[3]);
+    multiply<<<N, N>>>(N, D_TMP[0], D_IN[0], D_TMP[3]);
+    // BAB
+    // multiply(N, TMP[1], IN[1], TMP[4]);
+    multiply<<<N, N>>>(N, D_TMP[1], D_IN[1], D_TMP[4]);
+    // ABA+BAB
+    // add(N, TMP[3], TMP[4], TMP[5]);
+    add<<<N, N>>>(N, D_TMP[3], D_TMP[4], D_TMP[5]);
+
+    // #pragma omp parallel
+    // {
+        // D_ANS[tc][0] = signature(N, TMP[2]);
+        // D_ANS[tc][1] = signature(N, TMP[5]);
+        signature<<<1, 1>>>(N, D_TMP[2], &D_ANS[tc][0]);
+        signature<<<1, 1>>>(N, D_TMP[5], &D_ANS[tc][1]);
+    // }
  
+    cudaMemcpy(ANS[tc], D_ANS[tc], 2 * sizeof(UINT), cudaMemcpyDeviceToHost);
     return;
 }
 
@@ -88,7 +120,9 @@ int main() {
         tc++;
     }
 
-	omp_set_num_threads(MAXGPU);
+    int deviceCount = 0;
+    cudaGetDeviceCount(&deviceCount);
+	omp_set_num_threads(deviceCount);
 #pragma omp parallel for schedule(dynamic)
     for (int i=0; i<tc; i++) {
         cudaSetDevice(omp_get_thread_num());
@@ -96,7 +130,7 @@ int main() {
     }
 
     for (int i=0; i<tc; i++) {
-        printf("%u\n%u\n", D_ANS[i][0], D_ANS[i][1]);
+        printf("%u\n%u\n", ANS[i][0], ANS[i][1]);
     }
     return 0;
 }
